a418f00f6e4e2cead11fd2355b4c40d42ec1b4b9
valdoalmeida <72822380+valdoalmeida@users.noreply.github.com>
2023-09-23 09:12:30 -0300

    Add files via upload

--- modules/imgRandom.js
+++ modules/imgRandom.js
@@ -1,2 +1,18 @@
 
+let bgImage = document.getElementById("main");
+let imagens = [
+  "url(../img/img-main.webp)",
+  "url(../img/img-main2.webp)",
+  "url(../img/img-main3.webp)",
+  "url(../img/img-main4.webp)",
+  "url(../img/img-main5.webp)",
+];
 
+export function changeBgImagen() {
+  let indexImagen = Math.floor(Math.random() * imagens.length);
+
+  bgImage.style.backgroundImage = imagens[indexImagen];
+  bgImage.style.transition = "5s ease-in";
+}
+
+window.onload = setInterval(changeBgImagen, 8000);
--- modules/script.js
+++ modules/script.js
@@ -1,6 +1,6 @@
+import { changeBgImagen } from "../modules/imgRandom.js";
 import { abrirModal, fecharModal } from "../modules/modal.js";
 import { loadCounter } from "../modules/counter.js";
-
 
 
 
