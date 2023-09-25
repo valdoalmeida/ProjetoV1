d5110f4070651b86aa60e7a27cdd50a4991d6f35
valdoalmeida <72822380+valdoalmeida@users.noreply.github.com>
2023-09-25 09:47:34 -0300

    Add files via upload

--- modules/script.js
+++ modules/script.js
@@ -2,5 +2,3 @@ import { initialZoom } from "../modules/whatt.js";
 import { abrirModal, fecharModal } from "../modules/modal.js";
 import { loadCounter } from "../modules/counter.js";
 
-
-
---
+++ modules/scrollreveal.js
@@ -0,0 +1,9 @@
+
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.who-main', {duration: 2000});
+sr.reveal('.tec-work-main', {duration: 2000});
+sr.reveal('.tec-area-main', {duration: 2000});
+sr.reveal('.delivery-ours', {duration: 2000});
+
+
