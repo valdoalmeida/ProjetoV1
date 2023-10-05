f3de92560e650f4c2f0cc6cdf88eecb78539863b
valdoalmeida <72822380+valdoalmeida@users.noreply.github.com>
2023-10-05 09:21:33 -0300

    Add files via upload

--- modules/script.js
+++ modules/script.js
@@ -3,33 +3,40 @@ import { initialZoom } from "../modules/whatt.js";
 import { abrirModal, fecharModal } from "../modules/modal.js";
 import { loadCounter } from "../modules/counter.js";
 
-let yealink = document.querySelector(".img-container-yealink");
-let yealink1 = document.querySelector(".img-container-yealink1");
-let yealink2 = document.querySelector(".img-container-yealink2");
-let imagens = document.querySelectorAll(".img-container-yealink img");
-let imagens1 = document.querySelectorAll(".img-container-yealink1 img");
-let imagens2 = document.querySelectorAll(".img-container-yealink2 img");
+let containerYealink = document.getElementById("img-container-yealink");
+let containerYealink1 = document.getElementById("img-container-yealink1");
+let containerYealink2 = document.getElementById("img-container-yealink2");
+let yealink = document.querySelectorAll(".img-container-yealink img");
+let yealink1 = document.querySelectorAll(".img-container-yealink1 img");
+let yealink2 = document.querySelectorAll(".img-container-yealink2 img");
+let imgSuave = document.getElementById(".img-yealink");
 
-function rodarImagens() {
-  let numImagem = Math.floor(Math.random() * imagens.length);
-  yealink.innerHTML = `<img src="${imagens[numImagem].src}" alt="${imagens[numImagem].alt}">`;
-}
-function rodarImagens1() {
-  let numImagem = Math.floor(Math.random() * imagens.length);
-  yealink1.innerHTML = `<img src="${imagens1[numImagem].src}" alt="${imagens1[numImagem].alt}">`;
-}
-function rodarImagens2() {
-  let numImagem = Math.floor(Math.random() * imagens.length);
-  yealink2.innerHTML = `<img src="${imagens2[numImagem].src}" alt="${imagens2[numImagem].alt}">`;
+function mudarImagem() {
+  let mudarImg = Math.floor(Math.random() * yealink.length);
+  let mudarImg1 = Math.floor(Math.random() * yealink1.length);
+  let mudarImg2 = Math.floor(Math.random() * yealink2.length);
+
+  yealink.forEach((imagem) => {
+    imagem.style.opacity = 0;
+  });
+
+  yealink1.forEach((imagem) => {
+    imagem.style.opacity = 0;
+  });
+
+  yealink2.forEach((imagem) => {
+    imagem.style.opacity = 0;
+  });
+
+  containerYealink.innerHTML = `<img src="${yealink[mudarImg].src}" alt="${yealink[mudarImg].alt}">`;
+  containerYealink1.innerHTML = `<img src="${yealink1[mudarImg1].src}" alt="${yealink1[mudarImg1].alt}">`;
+  containerYealink2.innerHTML = `<img src="${yealink2[mudarImg2].src}" alt="${yealink2[mudarImg2].alt}">`;
 }
 
-function chamaImagem() {
-  rodarImagens();
-  rodarImagens1();
-  rodarImagens2();
+function chamarImagens() {
+  mudarImagem();
 }
 
-window.onload = () => {
- // Chama a função uma vez para exibir uma imagem inicial
-  setInterval(chamaImagem, 3000);
-};
+setInterval(() => {
+  mudarImagem();
+}, 2000);
